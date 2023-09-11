02efd7dddc9e1c36b9c3be841c96c79a569612b1
Pankaj Verma <36084571+pankaj610@users.noreply.github.com>
2023-09-11 23:18:26 +0530

    Update 51. Text Search.js

--- 51. Text Search.js
+++ 51. Text Search.js
@@ -3,15 +3,18 @@ export default function textSearch(string, query) {
     let regex = new RegExp(query); 
     let str = string.toLowerCase();
     let matches = str.matchAll(query.toLowerCase());
+    
     if(!query) {
       return string;
     }
     let i = 0;
+    
     for(const match of matches) {
         array.push(string.substring(i, match.index));
         array.push("<b>"+string.substring(match.index, match.index + query.length)+"</b>");
         i = match.index + query.length;
     }
+    
     array.push(string.substring(i));
     
     return array.join("");
